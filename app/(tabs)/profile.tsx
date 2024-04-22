e4d23e1101c7b653d278224c3987d0e05a9a8ebd
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-04-23 01:08:47 +0530

    feat: showing user data on profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,20 +1,200 @@
-import { View, Text, Button } from 'react-native';
-import React from 'react';
-import { useAuth } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import React, { useEffect, useState } from 'react';
+import { useAuth, useUser } from '@clerk/clerk-expo';
+import * as ImagePicker from 'expo-image-picker';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
+
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
 
 const ProfilePage = () => {
   const { signOut, isSignedIn } = useAuth();
+  const { user } = useUser();
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
+  const [edit, setEdit] = useState(false);
+
+  useEffect(() => {
+    if (!user) return;
+
+    setFirstName(user?.firstName);
+    setLastName(user?.lastName);
+    setEmail(user?.emailAddresses[0].emailAddress);
+  }, [user]);
+
+  const onSaveUser = async () => {
+    try {
+      if (!firstName || !lastName || !email) return;
+
+      await user?.update({
+        firstName,
+        lastName,
+      });
+    } catch (error) {
+      console.log('Error updating user:', error);
+    } finally {
+      setEdit(false);
+    }
+  };
+
+  const onCaptureImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 0.75,
+      base64: true,
+    });
+
+    if (!result.canceled) {
+      const base64 = `data:image/png;base64,${result.assets[0].base64}`;
+
+      await user?.setProfileImage({
+        file: base64,
+      });
+    }
+  };
+
   return (
-    <View>
-      <Button title="Log out" onPress={() => signOut()} />
-      {!isSignedIn && (
-        <Link href={'/(modals)/login'}>
-          <Text>Login</Text>
-        </Link>
+    <SafeAreaView style={defaultStyles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Profile</Text>
+        <Ionicons name="notifications-outline" size={26} color="black" />
+      </View>
+
+      {user && (
+        <View style={styles.card}>
+          <TouchableOpacity onPress={onCaptureImage}>
+            <Image source={{ uri: user?.imageUrl }} style={styles.avatar} />
+          </TouchableOpacity>
+          <View
+            style={{
+              flexDirection: 'row',
+              gap: 6,
+            }}
+          >
+            {edit ? (
+              <View style={styles.editRow}>
+                <TextInput
+                  value={firstName || ''}
+                  placeholder="First Name"
+                  onChangeText={setFirstName}
+                  style={[defaultStyles.inputField, { width: 100 }]}
+                />
+                <TextInput
+                  value={lastName || ''}
+                  placeholder="Last Name"
+                  onChangeText={setLastName}
+                  style={[defaultStyles.inputField, { width: 100 }]}
+                />
+                <TouchableOpacity onPress={onSaveUser}>
+                  <Ionicons
+                    name="checkmark-outline"
+                    size={24}
+                    color={Colors.dark}
+                  />
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View style={styles.editRow}>
+                <Text style={styles.name}>
+                  {firstName} {lastName}
+                </Text>
+                <TouchableOpacity onPress={() => setEdit(true)}>
+                  <Ionicons
+                    name="create-outline"
+                    size={24}
+                    color={Colors.dark}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+          <Text>{email}</Text>
+          <Text>Since {user?.createdAt?.toLocaleString()}</Text>
+        </View>
       )}
-    </View>
+
+      {isSignedIn && (
+        <View style={{ marginHorizontal: 24 }}>
+          <Button
+            title="Log out"
+            onPress={() => signOut()}
+            color={Colors.dark}
+          />
+        </View>
+      )}
+
+      {!isSignedIn && (
+        <View style={{ marginHorizontal: 24 }}>
+          <Link href={'/(modals)/login'} asChild>
+            <Button title="Log in" color={Colors.dark} />
+          </Link>
+        </View>
+      )}
+    </SafeAreaView>
   );
 };
 
 export default ProfilePage;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 24,
+    paddingTop: Platform.OS === 'android' ? 44 : 0,
+    gap: 10,
+  },
+  header: {
+    fontFamily: 'nunito-bold',
+    fontSize: 26,
+  },
+  card: {
+    backgroundColor: '#fff',
+    gap: 14,
+    alignItems: 'center',
+    padding: 24,
+    borderRadius: 8,
+    margin: 24,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    elevation: 5,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.primary,
+    marginBottom: 10,
+  },
+  editRow: {
+    height: 50,
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 8,
+  },
+  name: {
+    fontFamily: 'nunito-bold',
+    fontSize: 24,
+  },
+});
